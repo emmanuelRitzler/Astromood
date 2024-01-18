15ca30e891f95bfcbeb27f8c4e00d5ddec6ed32e
Emmanuel Ritzler <ritzler.emmanuel@gmail.com>
2024-01-18 16:49:44 +0100

    changes layout + submit JS

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const getName = () => {
-    const getName = document.querySelector('userName').value;
+let message = document.querySelector("#personalisedMessage");
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('.btn-form-submit');
+    form.addEventListener('click', (event) => {
+        event.preventDefault();
+        let userName = document.getElementById('userName').value;
+        let userBirthday = document.getElementById('userBirthday').value;
+        let mood = document.querySelector('input[name="mood"]:checked').value; 
+        const userHoroscope = `${userName}, votre signe astrologique est ${userBirthday}. Vu votre humeur du jour, votre prédiction est ${mood}`;
+        message.innerHTML = userHoroscope;
+    })
+});
+
+const sadQuote = [];
+const happyQuote = [];
+const angryQuote = []; 
+
+
+
+const GetRandomNumber = () => {
+    return Math.floor(Math.random() * 9)
 };
-
-const getBirthday = () => {
-    document.querySelector('userBirthday').value;
-};
-
-const userName = getName();
-const birthDate = getBirthday();
-
-console.log(userName);
-console.log(birthDate);
+// console.log(GetRandomNumber()); -- testé et approuvé
