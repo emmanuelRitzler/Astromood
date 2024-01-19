523d2fe81e22451a6de20afa56675a5eebe159b4
Emmanuel Ritzler <ritzler.emmanuel@gmail.com>
2024-01-19 11:35:34 +0100

    Add quote function JS

--- index.js
+++ index.js
@@ -1,50 +1,80 @@
-/* INSERT MESSAGE INTO CONTAINER */
+/* GET RANDOM QUOTE IN AN ARRAY OF QUOTES */
+
+const GetRandomNumber = () => {
+    return Math.floor(Math.random() * 3)
+};
+const randomIndex = GetRandomNumber();
+// console.log(randomIndex);
+
+const allQuotes = {
+    sadQuote: ["vous avez besoin d'un plaid et d'une boisson chaude.", "vous allez écoutez des titres de votre playlist préférée pour vous remonter le moral.", "vous allez appeler un proche pour vous confiez."],
+    happyQuote: ["vous allez faire une rencontre inoubliable.", "vous allez passer une excellente journée.", "vous allez embellir la journée d'une personne proche."],
+    angryQuote: ["vous allez retrouver le sourire en voyant un proche que vous aimez.", "vous allez vous défoulez en faisant du sport.", "vous allez attendre que l'orage passe puis retrouver le soleil."]
+};
 
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('.btn-form-submit');
     form.addEventListener('click', (event) => {
-        event.preventDefault();
-        let userName = document.getElementById('userName').value;
-        let userBirthday = document.getElementById('userBirthday').value;
-        let mood = document.querySelector('input[name="mood"]:checked').value;
-        let message = document.querySelector("#personalisedMessage"); 
-        const userHoroscope = `${userName}, votre signe astrologique est ${userBirthday}. Vu votre humeur du jour, votre prédiction est ${mood}`;
-        message.innerHTML = userHoroscope;
-    })
+        const getMood = () => {
+            event.preventDefault();
+            let chosenMood = document.querySelector('input[name="mood"]:checked').value;
+            if (chosenMood === 'good-mood') {
+                chosenMood = allQuotes.happyQuote[randomIndex];
+                return chosenMood;
+            }
+            else if (chosenMood === 'so-so-mood') {
+                chosenMood = allQuotes.sadQuote[randomIndex];
+                return chosenMood;
+            }
+            else if (chosenMood === 'bad-mood') {
+                chosenMood = allQuotes.angryQuote[randomIndex];
+                return chosenMood;
+            }
+            else {
+                console.log('Choisissez votre humeur');
+            }
+        };   
+
+        const theQuote = getMood();
+    
+        const testUserHoroscope = `Vu votre humeur du jour, votre prédiction est que ${theQuote}`;
+        console.log(testUserHoroscope);
+    });
 });
 
-console.log(userBirthday);
 
-/* GET RANDOM QUOTE IN AN ARRAY OF QUOTES */
+/* INSERT MESSAGE INTO CONTAINER */
 
-const sadQuote = [];
-const happyQuote = [];
-const angryQuote = []; 
-
-
-
-const GetRandomNumber = () => {
-    return Math.floor(Math.random() * 9)
-};
-// console.log(GetRandomNumber()); -- testé et approuvé
+// document.addEventListener('DOMContentLoaded', function() {
+//     const form = document.querySelector('.btn-form-submit');
+//     form.addEventListener('click', (event) => {
+//         event.preventDefault();
+//         let userName = document.getElementById('userName').value;
+//         let userBirthday = document.getElementById('userBirthday').value;
+//         let mood = document.querySelector('input[name="mood"]:checked').value;
+//         console.log(mood);
+//         let message = document.querySelector("#personalisedMessage"); 
+//         const userHoroscope = `${userName}, votre signe astrologique est ${userBirthday}. Vu votre humeur du jour, votre prédiction est que ${mood}`;
+//         message.innerHTML = userHoroscope;
+//     })
+// });
 
 /* GET ASTRO SIGN */
 
-const listAstroSigns = {
-    virgo: 'vierge',
-    aries: 'bélier',
-    taurus: 'taureau',
-    gemini: 'gémeaux',
-    cancer: 'cancer',
-    leo: 'lion',
-    libra: 'balance',
-    scorpio: 'scorpion',
-    sagittarius: 'sagittaire',
-    capricorn: 'capricorne',
-    pisces: 'poisson',
-    aquarius: 'verseau'
-}
-
+// const listAstroSigns = {
+//     virgo: 'vierge',
+//     aries: 'bélier',
+//     taurus: 'taureau',
+//     gemini: 'gémeaux',
+//     cancer: 'cancer',
+//     leo: 'lion',
+//     libra: 'balance',
+//     scorpio: 'scorpion',
+//     sagittarius: 'sagittaire',
+//     capricorn: 'capricorne',
+//     pisces: 'poisson',
+//     aquarius: 'verseau'
+// }
 
 /* CHECK IF USER INPUT IS VALID */
 
